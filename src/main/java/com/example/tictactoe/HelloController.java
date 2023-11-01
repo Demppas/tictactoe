6a585f22d994e2db06b6f341b102dca1f70121c9
demppas <dennis.staffansson@iths.se>
2023-11-01 20:23:30 +0100

    added buttons

--- src/main/java/com/example/tictactoe/HelloController.java
+++ src/main/java/com/example/tictactoe/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.tictactoe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label colorText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onColorButtonAction() {
+        colorText.setText("Choose color");
+    }
+
+    public void onLabelAction(MouseEvent mouseEvent) {
+        colorText.setText("");
+    }
+
+    public void onStartButtonAction(ActionEvent actionEvent) {
+        //start the game
+    }
+
+    public void onChooseDifficultyAction(ActionEvent actionEvent) {
     }
 }
